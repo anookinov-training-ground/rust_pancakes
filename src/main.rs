4892d28ae2b41daedad7cc95b15b21a6f50bdd89
Alexander Nookinov <anookinov@gmail.com>
2021-09-04 20:57:45 +0700

    add attribute-like and function-like macros

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,16 @@ impl HelloMacro for Pancakes {
 fn main() {
     Pancakes::hello_macro();
 }
+
+// Attribute-like macros
+// #[route(GET, "/")]
+// fn index() {}
+
+// #[proc_macro_attribute]
+// pub fn route(attr: TokenStream, item: TokenStream) -> TokenStream {}
+
+// Function-like macros
+// let sql = sql!(SELECT * FROM posts WHERE id=1);
+
+// #[proc_macro]
+// pub fn sql(input: TokenStream) -> TokenStream {}
